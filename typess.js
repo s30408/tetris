deb1ff26d27ae47c1b8e2b6a96d5754de63f977c
s30408 <45545124+s30408@users.noreply.github.com>
2019-03-12 23:15:45 +0900

    ver.1

---
+++ typess.js
@@ -0,0 +1,182 @@
+﻿var types = {};
+types.type1 = {
+	"name" : "type1",
+	"width" : 2,
+	"height" : 2,
+	"maxRotation" : 1,
+	"shape" : 	[[1,1],
+        			 [1,1]]
+};
+types.type2 = {
+	"name" : "type2",
+	"width" : 3,
+	"height" : 3,
+	"maxRotation" : 4,
+	"shape" : 	[[0,1,0],
+        			 [1,1,1],
+        			 [0,0,0]]
+};
+types.type3 = {
+	"name" : "type3",
+	"width" : 4,
+	"height" : 4,
+	"maxRotation" : 2,
+	"shape" : 	[[0,1,0,0],
+        			 [0,1,0,0],
+        			 [0,1,0,0],
+        			 [0,1,0,0]]
+};
+types.type4 = {
+	"name" : "type4",
+	"width" : 3,
+	"height" : 3,
+	"maxRotation" : 4,
+	"shape" : 	[[0,1,0],
+        			 [0,1,0],
+        			 [0,1,1]]
+};
+types.type5 = {
+	"name" : "type5",
+	"width" : 3,
+	"height" : 3,
+	"maxRotation" : 4,
+	"shape" : 	[[0,1,0],
+        			 [0,1,0],
+        			 [1,1,0]]
+};
+types.type6 = {
+	"name" : "type6",
+	"width" : 3,
+	"height" : 3,
+	"maxRotation" : 2,
+	"shape" : 	[[1,0,0],
+        			 [1,1,0],
+        			 [0,1,0]]
+};
+types.type7 = {
+	"name" : "type7",
+	"width" : 3,
+	"height" : 3,
+	"maxRotation" : 2,
+	"shape" : 	[[0,1,0],
+        			 [1,1,0],
+        			 [1,0,0]]
+};
+
+types.type8 = {
+	"name" : "type8",
+	"width" : 4,
+	"height" : 4,
+	"maxRotation" : 4,
+	"shape" : 	[[0,1,0,0],
+        			 [1,1,0,0],
+        			 [1,0,0,0],
+						   [1,0,0,0]]
+};
+
+types.type9 = {
+	"name" : "type9",
+	"width" : 3,
+	"height" : 3,
+	"maxRotation" : 4,
+	"shape" : 	[[1,0,0],
+        			 [1,0,0],
+						   [1,1,1]]
+};
+
+types.type10 = {
+	"name" : "type10",
+	"width" : 4,
+	"height" : 4,
+	"maxRotation" : 4,
+	"shape" : 	[[1,1,0,0],
+        			 [1,1,0,0],
+        			 [1,0,0,0],
+						   [1,0,0,0]]
+};
+
+types.type11 = {
+	"name" : "type11",
+	"width" : 5,
+	"height" : 5,
+	"maxRotation" : 2,
+	"shape" : 	[[1,0,0,0,0],
+        			 [1,0,0,0,0],
+        			 [1,0,0,0,0],
+							 [1,0,0,0,0],
+						   [1,0,0,0,0]]
+};
+
+types.type12 = {
+	"name" : "type12",
+	"width" : 3,
+	"height" : 3,
+	"maxRotation" : 4,
+	"shape" : 	[[0,1,0],
+        			 [0,1,0],
+						   [1,1,1]]
+};
+
+types.type13 = {
+	"name" : "type13",
+	"width" : 4,
+	"height" : 4,
+	"maxRotation" : 4,
+	"shape" : 	[[1,0,0,0],
+        			 [1,1,0,0],
+        			 [1,0,0,0],
+							 [1,0,0,0]]
+};
+
+types.type14 = {
+	"name" : "type14",
+	"width" : 4,
+	"height" : 4,
+	"maxRotation" : 4,
+	"shape" : 	[[1,0,0,0],
+       			 	 [1,0,0,0],
+       			 	 [1,1,0,0],
+						 	 [1,0,0,0]]
+};
+
+types.type15 = {
+	"name" : "type15",
+	"width" : 4,
+	"height" : 4,
+	"maxRotation" : 4,
+	"shape" : 	[[1,0,0,0],
+        			 [1,0,0,0],
+        			 [1,1,0,0],
+						   [1,1,0,0]]
+};
+
+types.type16 = {
+	"name" : "type16",
+	"width" : 4,
+	"height" : 4,
+	"maxRotation" : 4,
+	"shape" : 	[[1,0,0,0],
+        			 [1,0,0,0],
+        			 [1,1,0,0],
+						   [0,1,0,0]]
+};
+
+types.type17 = {
+	"name" : "type17",
+	"width" : 3,
+	"height" : 3,
+	"maxRotation" : 4,
+	"shape" : 	[[0,1,0],
+        			 [1,1,0],
+        			 [1,1,0]]
+};
+
+types.type18 = {
+	"name" : "type18",
+	"width" : 3,
+	"height" : 3,
+	"maxRotation" : 4,
+	"shape" : 	[[1,0,0],
+        			 [1,1,0],
+        			 [1,1,0]]
+};
